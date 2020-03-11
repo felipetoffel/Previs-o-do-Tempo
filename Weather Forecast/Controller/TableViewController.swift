47b2aab85d664e0228ce94858acdaca890dd7229
Jonathan <jonathan@Jonathans-iMac.local>
2020-03-11 11:52:46 -0300

    W/P 11/03/2020 - correção ao consumir as informações da API Weather

--- Weather Forecast/Controller/TableViewController.swift
+++ Weather Forecast/Controller/TableViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  TableViewController.swift
 //  Weather Forecast
 //
-//  Created by Jonathan on 09/03/20.
+//  Created by Jonathan on 10/03/20.
 //  Copyright © 2020 teste. All rights reserved.
 //
 
@@ -11,20 +11,55 @@ import Alamofire
 import AlamofireImage
 import SwiftyJSON
 
-class ViewController: UITableViewController {
+class TableViewController: UITableViewController {
     
-    var places = [Place]()
+    var places: [Place] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-       
-        
-        
         loadCity()
         
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return places.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //as? cell
+        let cellIdentifier = "cityPlace"
+        
+        guard let cells = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? cell  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+            }
+        let place = places[indexPath.row]
+            
+        cells.namePlace.text = place.name
+        cells.descriptionPlace.text = place.description
+        cells.celsius.text = place.temperature
+        
+        // Configure the cell...
+
+        return cells
     }
     
+
     func loadCity(){
         listCity(city: "São Paulo") {
             (response : Data) in
@@ -65,15 +100,16 @@ class ViewController: UITableViewController {
             
         }
         
+        
     }
-
+    
     func listCity (city: String, completion: @escaping (_ response: Data) -> Void){
-        let url = "http://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=7c189bba4bca238455291a386f2294b5"
+        let cidade = city.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=\(cidade!)&appid=7c189bba4bca238455291a386f2294b5"
         Alamofire.request(url).responseJSON{ response in
-            completion(response.result.value as! Data)
+            completion(response.result as! Data)
             //let name = "q?"
         }
     }
 
 }
-
